af68647ebad5b9624f478d5afbfcdbdf5c76d803
Jonathan-Skeete <Jonathan.Skeete.dev@gmail.com>
2024-04-02 16:32:05 -0400

    fixed typos

--- Python/temperature.py
+++ Python/temperature.py
@@ -6,16 +6,16 @@
     *   If the second parameter is false, convert the temperature from Celsius to Fahrenheit.
 """
 
-def ConvertTemperture(temperature: float, isFahrenheit: bool) -> float:
+def ConvertTemperature(temperature: float, isFahrenheit: bool) -> float:
     ...
     # Celsius = (Fahrenheit - 32) * 5/9
     # Fahrenheit = Celsius * 9/5 + 32
     # Write your code here
 
 def main() -> None:
-    print(f"32°F is {ConvertTemperture(32, True)}°C") # 0°C
-    print(f"100°C is {ConvertTemperture(100, False)}°F") # 212°F
-    print(f"98.6°F is {ConvertTemperture(98.6, True)}°C") # 37°C
+    print(f"32°F is {ConvertTemperature(32, True)}°C") # 0°C
+    print(f"100°C is {ConvertTemperature(100, False)}°F") # 212°F
+    print(f"98.6°F is {ConvertTemperature(98.6, True)}°C") # 37°C
 
 if __name__ == '__main__':
     main()
